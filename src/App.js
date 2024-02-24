62bc22ab99cf2f9235c717e7c7c35ba3922e360a
Binit kc <077bct017.binit@pcampus.edu.np>
2024-02-24 21:32:01 +0545

    successfully uploaded image to backend using prac.js now , trying to build one myself with necessary attributes.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/Navbar';
 import AppCard from './components/Card';
+import Prac from './components/Prac';
 
 function App() {
   return (
     <div>
       <AppNavbar />
-      <AppCard />
+      {/* <AppCard /> */}
+      <Prac />
     </div>
   );
 }
---
+++ src/components/Prac.js
@@ -0,0 +1,38 @@
+import { upload } from '@testing-library/user-event/dist/upload';
+import React from 'react'
+import { useState } from 'react';
+
+const Prac = () => {
+
+    const[ title, setTitle ] = useState("");
+    const[ cover, setCover ] = useState();
+
+    const newBook = ()=>{
+        const uploadData = new FormData();
+        uploadData.append('title', title);
+        uploadData.append('cover', cover, cover.name); 
+        console.log(title);
+        fetch('http://127.0.0.1:8000/books/',
+            { method: 'POST',
+              body:uploadData 
+            }).then( res => console.log(res) ).catch(error => console.log(error))
+    }
+
+  return (
+    <div>
+        <h3>UPLOAD images with React</h3>
+        <label htmlFor="">
+            Title
+            <input type="text" value={title} onChange={(evt) => setTitle(evt.target.value)}/> 
+        </label>
+        <br />
+        <label htmlFor="">
+            Cover
+            <input type="file" onChange={(evt) => setCover(evt.target.files[0])} />
+        </label>
+        <button onClick={() => newBook()}>New Book</button>
+    </div>
+  )
+}
+
+export default Prac
