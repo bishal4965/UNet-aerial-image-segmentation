e85183bea292df3a3956c8eb93b32c5cd51cd10c
Bishal Aryal <077BCT021.bishal@gmail.com>
2024-03-25 20:52:02 +0545

    Updated Code!

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/Card.css'
 
 function AppCard() {
+  const classColorMap = {
+    "Building": [60, 16, 152],
+    "Land": [132, 41, 246],
+    "Road": [110, 193, 228],
+    "Vegetation": [254, 221, 58],
+    "Water": [226, 169, 41],
+    "Unlabeled": [155, 155, 155]
+  };
+
   const [unMaskedImage, setUnMaskedImage] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
   const [maskImageUrl, setMaskImageUrl] = useState("");
+  const [classAreas, setClassAreas] = useState(null);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -14,6 +24,7 @@ function AppCard() {
   };
 
   const sendImage = () => {
+    if (!unMaskedImage) return; // Check if unMaskedImage is set
     const uploadData = new FormData();
     uploadData.append("unMaskedImage", unMaskedImage, unMaskedImage.name);
 
@@ -21,9 +32,16 @@ function AppCard() {
       .then(response => response.json())
       .then(data => {
         setMaskImageUrl(data.predictedMaskUrl);
+        setClassAreas(data.classAreas);
       })
       .catch(error => console.log(error));
   };
+
+  useEffect(() => {
+    if (classAreas) {
+      console.log(classAreas);
+    }
+  }, [classAreas]);
 
   return (
     <div className={`container ${imageUrl ? 'image-uploaded' : ''}`}>
@@ -52,15 +70,25 @@ function AppCard() {
             </div>
             <div className="col-md-6">
               {maskImageUrl && (
-                <div>
-                  <h6 className="card-subtitle mb-2 text-muted">Predicted Mask</h6>
-                  <img src={maskImageUrl} className="card-img-top mb-3" alt="Predicted Mask" style={{ 
-                    maxWidth: "100%", 
-                    maxHeight: "100%", 
-                    width: "auto", 
-                    height: "auto" 
-                  }} 
-                  />
+                <div className="d-flex justify-content-center align-items-center">
+                  <div className="mb-3">
+                    <h6 className="card-subtitle mb-2 text-muted">Predicted Mask</h6>
+                    <img src={maskImageUrl} className="card-img-top mb-3" alt="Predicted Mask" style={{ 
+                      maxWidth: "100%", 
+                      maxHeight: "100%", 
+                      width: "auto", 
+                      height: "auto" 
+                    }} 
+                    />
+                  </div>
+                  <div className="class-color-map">
+                    {Object.entries(classColorMap).map(([className, color]) => (
+                      <div key={className} className="class-color-item">
+                        <div className="color-rectangle" style={{ backgroundColor: `rgb(${color.join(", ")})` }}></div>
+                        <p className="class-name">{className}: {classAreas ? classAreas[className].toFixed(2) + "%" : ""}</p>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               )}
             </div>
